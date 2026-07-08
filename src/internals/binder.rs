11a2e486a45964ffe681112ed7b584d68a786838
Metehan120 <metehanzafer@pm.me>
2026-07-08 04:09:25 +0300

    Update binder.rs

--- src/internals/binder.rs
+++ src/internals/binder.rs
@@ -1,6 +1,6 @@
 use std::os::raw::c_void;
 
-use syscalls::{Sysno, syscall6};
+use syscalls::{Sysno, syscall};
 
 const MPOL_PREFERRED: i32 = 1;
 const MPOL_F_STATIC_NODES: usize = 1 << 15;
@@ -18,14 +18,14 @@ pub unsafe fn prefer_node(ptr: *mut c_void, len: usize, node_id: u16) -> bool {
     let mask = node_mask_word(node_id);
     let maxnode = node_id as usize + 1;
 
-    syscall6(
+    syscall!(
         Sysno::mbind,
         ptr as usize,
         len,
         MPOL_PREFERRED as usize,
         &mask as *const usize as usize,
         maxnode,
-        MPOL_F_STATIC_NODES,
+        MPOL_F_STATIC_NODES
     )
     .is_ok()
 }

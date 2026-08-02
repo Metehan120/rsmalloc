e783baf09f6b086ee4e375dc3e5ca8952fec7cae
Metehan120 <metehanzafer@pm.me>
2026-08-02 12:14:06 +0300

    small feature gate for trimmer

--- src/trim.rs
+++ src/trim.rs
@@ -151,7 +151,9 @@ pub unsafe fn trim_small(requested_size: usize) -> usize {
             };
 
             if output.is_null() {
-                TRIM_PREDICTOR[class].update_refill(10000, 100, 10000);
+                if !cfg!(feature = "trim-aggressively") {
+                    TRIM_PREDICTOR[class].update_refill(10000, 100, 10000);
+                }
                 continue;
             }
 
